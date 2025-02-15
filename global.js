8bc6a98bfb1dfe4a0e8ef93c1313f1edd2a990b8
unknown <linybao1@gmail.com>
2025-02-14 22:37:11 -0800

    delete csv

--- global.js
+++ global.js
@@ -147,6 +147,3 @@ export function renderProjects(project, containerElement, headingLevel = 'h2') {
   export async function fetchGithubData(username) {
     return fetchJSON(`https://api.github.com/users/${username}`);
 }
-
-
-~
